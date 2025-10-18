cf65642f68577a4be34e29958b27ddf040ad18fa
Njeri Njoroge <129849647+njer1nj0r0ge236@users.noreply.github.com>
2025-10-19 01:20:37 +0300

    app.py

--- app.py
+++ app.py
@@ -27,8 +27,7 @@ if page == "Home":
     st.title("Telco Customer Churn Prediction App")
     st.image("https://raw.githubusercontent.com/njer1nj0r0ge236/Telco-dataset-customer-churn-prediction-classification-analysis/main/Image%20Aug%2020%2C%202025%2C%2001_06_30%20AM.png")
     st.write(
-        """
-        Welcome! 👋  
+        """  
         This app predicts customer churn based on various features such as gender, contract method, service usage.  
         
         ### 📌 Overview
@@ -328,6 +327,7 @@ elif page == "History":
     else:
 
         st.info("No prediction history found yet. Make some predictions on the 'Predict' page to view a history.")
+
 
 
 
